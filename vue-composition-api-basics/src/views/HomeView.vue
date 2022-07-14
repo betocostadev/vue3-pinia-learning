cc299dd75649a3cfead394cfa42a3271a6a5e582
betocostadev <robertomarx@gmail.com>
2022-07-14 19:49:59 -0300

    added some more reactive and non reactive data

--- vue-composition-api-basics/src/views/HomeView.vue
+++ vue-composition-api-basics/src/views/HomeView.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>{{ appTitle }}</h1>
 
     <h3>{{ counterTitle }}</h3>
     <div>
       <button class="btn" @click="decreaseCounter">-</button>
       <span class="counter">{{ counter }}</span>
       <button class="btn" @click="increaseCounter">+</button>
+    </div>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterTitle" text="input" />
+    </div>
+
+    <h3 class="second-counter-title">{{ otherCounterData.title }}</h3>
+    <div>
+      <button class="btn" @click="decreaseCounterTwo(10, $event)">- 10</button>
+      <button class="btn" @click="decreaseCounterTwo(1, $event)">- 1</button>
+      <span class="counter">{{ otherCounterData.count }}</span>
+      <button class="btn" @click="increaseCounterTwo(1)">+ 1</button>
+      <button class="btn" @click="increaseCounterTwo(10)">+ 10</button>
     </div>
   </div>
 </template>
@@ -45,19 +59,42 @@ export default {
 </script> -->
 
 <script setup>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
+
+// Use non-reactive Data for best performance if you don't need it to change.
+const appTitle = 'My Amazing Counter App'
 
 const counterTitle = ref('Setup counter'),
   counter = ref(0)
 
+const otherCounterData = reactive({
+  title: 'The second Counter',
+  count: 0,
+})
+
 const increaseCounter = () => counter.value++
 const decreaseCounter = () => counter.value--
+
+const increaseCounterTwo = (amount) => (otherCounterData.count += amount)
+// Passing also the $event
+const decreaseCounterTwo = (amount, evt) => {
+  console.log(evt)
+  otherCounterData.count -= amount
+}
 </script>
 
 <style>
 .home {
   text-align: center;
   padding: 20px;
+}
+
+.edit {
+  margin-top: 50px;
+}
+
+.second-counter-title {
+  margin-top: 30px;
 }
 
 .btn,
