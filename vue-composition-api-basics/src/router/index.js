49176c09c977df37a54a5f5316ee0d20241487db
betocostadev <robertomarx@gmail.com>
2022-07-15 09:01:53 -0300

    adding routes

--- vue-composition-api-basics/src/router/index.js
+++ vue-composition-api-basics/src/router/index.js
@@ -7,17 +7,22 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/posts',
+      name: 'posts',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+      component: () => import('../views/PostsView.vue'),
+    },
+    {
+      path: '/postDetail/:id',
+      name: 'postDetail',
+      component: () => import('../views/PostDetailView.vue'),
+    },
+  ],
 })
 
 export default router
---
+++ vue-composition-api-basics/src/views/PostDetailView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="post-detail">
+    <h2>Post Detail</h2>
+    <p>Contents of the post {{ $route.params.id }}</p>
+    <p class="btn-back"><RouterLink to="/posts">&lt; Back</RouterLink></p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.btn-back {
+  margin-top: 2rem;
+}
+</style>
