8950aa72a55e730e2208654e32891d3a198ceef6
betocostadev <robertomarx@gmail.com>
2022-07-16 15:46:12 -0300

    #2811557550 - added some more router examples

--- vue-composition-api-basics/src/views/PostDetailView.vue
+++ vue-composition-api-basics/src/views/PostDetailView.vue
@@ -2,11 +2,28 @@
   <div class="post-detail">
     <h2>Post Detail</h2>
     <p>Contents of the post {{ $route.params.id }}</p>
+    <p>This post ID is {{ postId }}</p>
+    <div>
+      <button class="btn-primary" @click="getPostId">Get Post ID</button>
+    </div>
+    <button class="btn-primary" @click="goHome">Go Home</button>
     <p class="btn-back"><RouterLink to="/posts">&lt; Back</RouterLink></p>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const postId = ref('')
+
+const route = useRoute()
+const router = useRouter()
+
+const getPostId = () => (postId.value = route.params.id)
+
+const goHome = () => router.push({ name: 'home' })
+</script>
 
 <style scoped>
 .btn-back {
