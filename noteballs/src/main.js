c337bde0740cab0555def9b2018a2783ee4ae511
betocostadev <robertomarx@gmail.com>
2022-07-20 18:36:16 -0300

    started noteballs

---
+++ noteballs/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import './assets/main.css'
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [],
+})
+
+createApp(App).use(router).mount('#app')
